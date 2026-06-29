ff5870986d66b7d42213a11ee6f305c5fbcbc8bb
ManoelDev-ifma <lucas.reis.ufma@gmail.com>
2026-06-29 17:20:01 -0300

    feat: create side nav component

--- src/Components/Sidenav.tsx
+++ src/Components/Sidenav.tsx
@@ -1,5 +1,55 @@
+import Vendas from "../assets/icons/vendas.svg";
+import Resumo from "../assets/icons/resumo.svg";
+import Webhooks from "../assets/icons/webhooks.svg";
+import Configuracoes from "../assets/icons/configuracoes.svg";
+import Contato from "../assets/icons/contato.svg";
+import Sair from "../assets/icons/sair.svg";
+import FintechSVG from "../assets/FintechSVG";
+
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Fintech" />
+      <ul>
+        <li>
+          <span>
+            <img src={Resumo} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={Vendas} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={Webhooks} alt="" />
+          </span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={Configuracoes} alt="" />
+          </span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={Contato} alt="" />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={Sair} alt="" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidenav;
